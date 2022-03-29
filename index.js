f00a4dc43f50eb2846278a3708cd6773884becf3
Ryan <ryan.webster@student.curtin.edu.au>
2022-03-29 19:36:55 +0800

    added prettier for formatting

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 
-// Initialise new instance of socket.io by passing 
+// Initialise new instance of socket.io by passing
 // the server object
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+//
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-
 // listen for connection events and log to console when new connection occurs
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
   //Listen for disconnection events
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 
   //Listen for chat message events
-  socket.on('chat message', (msg) => {
-    console.log('new message: ' + msg);
+  socket.on("chat message", (msg) => {
+    // emit is used to send messages to every connected user
+    io.emit("chat message", msg);
+
+    console.log("new message: " + msg);
   });
 });
 
-
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
