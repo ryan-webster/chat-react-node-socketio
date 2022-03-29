eb187888cabc52688bcecde0ade9f98cec1bade8
Ryan <ryan.webster@student.curtin.edu.au>
2022-03-29 19:13:50 +0800

    added disconnect/connect listeners

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Initialise new instance of socket.io by passing 
+// the server object
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+// listen for connection events and log to console when new connection occurs
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  //Listen for disconnection events
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
